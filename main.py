0590324711764dba8dc60b740f42f5714b735123
Ricardo Matos <matosricardordg@gmail.com>
2025-10-07 13:54:36 -0300

    feat: add line graph to widget
      Implement a line graph to plot numeric data and solve dependencies
      problems to convert the designer file to a py file. Run the interface
      though the main file.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import sys
 from data_access import DataAccess
-from cli import show_all_data
+from cli import MainWindow
+from PyQt5.QtWidgets import QApplication
 
+from ui import Ui_Graph
 
 def main():
 	data_access = DataAccess
 	data = data_access.get_all_data()
-	show_all_data(data)
+	#show_all_data(data)
 
 
-main()
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
